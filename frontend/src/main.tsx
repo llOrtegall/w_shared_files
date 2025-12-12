5b2642110931092ba8b2443ea968b251534a57c2
llOrtegall <IvanOrtega_97@hotmail.com>
2025-12-12 16:05:45 -0500

    add axios to get enviroment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,10 @@ import { RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { router } from './routes';
+import axios from 'axios';
 import './index.css';
+
+axios.defaults.baseURL= import.meta.env.VITE_API_URL ?? "/api/v1"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
