e06f05737f0cf6a742085cbb53d11c96de8ad320
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-02 17:38:14 -0500

    refactor: enhance UploadPage component with additional upload state management

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -4,7 +4,17 @@ import ErrorMessage from '../components/ui/ErrorMessage';
 import { useFileUpload } from '../hooks/useFileUpload';
 
 export default function UploadPage() {
-  const { uploadFile, downloadLink, isUploading, error, progress, resetUpload } = useFileUpload();
+  const {
+    uploadFile,
+    downloadLink,
+    isUploading,
+    isCancelling,
+    isMultipartUpload,
+    error,
+    progress,
+    cancelUpload,
+    resetUpload,
+  } = useFileUpload();
 
   const handleFileSelect = (file: File) => {
     uploadFile(file);
@@ -15,15 +25,24 @@ export default function UploadPage() {
   };
 
   return (
-    <>
+    <section className=''>
       {error && <ErrorMessage message={error} onDismiss={resetUpload} />}
 
       <section className="flex flex-col items-center gap-6">
         {!downloadLink
-          ? <UploadZone onFileSelect={handleFileSelect} isUploading={isUploading} progress={progress} />
+          ? (
+            <UploadZone
+              onFileSelect={handleFileSelect}
+              isUploading={isUploading}
+              isCancelling={isCancelling}
+              isMultipartUpload={isMultipartUpload}
+              onCancel={cancelUpload}
+              progress={progress}
+            />
+          )
           : <UploadSuccess downloadLink={downloadLink} onUploadAgain={handleUploadAgain} />
         }
       </section>
-    </>
+    </section>
   );
 }
