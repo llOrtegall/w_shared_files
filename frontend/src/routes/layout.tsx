bd802d3657780be4972625fea05e70258ff87818
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-02 17:38:20 -0500

    fix: update layout component with correct branding and social media links

--- frontend/src/routes/layout.tsx
+++ frontend/src/routes/layout.tsx
@@ -6,8 +6,10 @@ export default function Layout() {
 
       <nav className="w-full flex justify-between px-12 py-2">
         <div className="flex items-center gap-2">
-          <figure className="p-2 size-6 rounded-md bg-gray-200"></figure>
-          <span className="font-imb-400 text-gray-300 ">wSend</span>
+          <figure>
+            <img src="/Logo.svg" alt="wShare Logo" className="size-7" />
+          </figure>
+          <span className="font-imb-400 text-gray-300 ">wShare</span>
         </div>
         <div className="flex gap-4 font-imb-400 text-gray-300">
           <button disabled>Login</button>
@@ -15,19 +17,44 @@ export default function Layout() {
         </div>
       </nav>
 
-      <main className="flex items-center justify-center">
+      <main className="flex items-center justify-center relative">
         <Outlet />
       </main>
 
       <footer className="w-full flex justify-between px-12 py-2">
         <div className="font-imb-400 text-gray-1">
-          wSender | winkermind © 2025
+          <span>wShare | </span>
+          <a
+            href='https://winkermind.com/'
+            className="hover:text-green-400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >winkermind©
+          </a>
+          <span> 2026</span>
         </div>
         <div className="flex gap-4 font-imb-400 text-gray-1">
-          <span>FB</span>
-          <span>TW</span>
-          <span>IG</span>
-          <span>IN</span>
+          <a className="rounded-full bg-green-4 size-8 flex items-center justify-center cursor-pointer hover:bg-green-500"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/winkermind/"
+          >
+            <img src="/ig.png" alt="Instagram Logo" className="size-5" />
+          </a>
+          <a className="rounded-full bg-green-4 size-8 flex items-center justify-center cursor-pointer hover:bg-green-500"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/winkermind"
+          >
+            <img src="/fb.png" alt="Facebook Logo" className="size-5" />
+          </a>
+          <a className="rounded-full bg-green-4 size-8 flex items-center justify-center cursor-pointer hover:bg-green-500 text-black"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://whatsapp.com/channel/0029Va4on5GJJhzfsxIren2m"
+          >
+            WA
+          </a>
         </div>
       </footer>
     </section>
